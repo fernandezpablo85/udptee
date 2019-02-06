89529b0dfe4c32bf2f9cae8e9df3f39a1415983c
Sebastian Waisbrot <seppo0010@gmail.com>
2019-02-06 18:00:23 -0300

    Add more comments and gofmt

--- udp.go
+++ udp.go
@@ -6,14 +6,15 @@ import (
 	"net"
 )
 
-func udpConnect(host string, port int) (net.Conn, error) {
+// UDPConnect connects to the given host and port
+func UDPConnect(host string, port int) (net.Conn, error) {
 	connstr := fmt.Sprintf("%s:%d", host, port)
 	return net.Dial("udp", connstr)
 }
 
 // MustUDPConnect connects to the given host and port or crashes the program
 func MustUDPConnect(host string, port int) net.Conn {
-	conn, err := udpConnect(host, port)
+	conn, err := UDPConnect(host, port)
 	if err != nil {
 		log.Fatalf("error while connecting to %s:%d: %v", host, port, err)
 	}
